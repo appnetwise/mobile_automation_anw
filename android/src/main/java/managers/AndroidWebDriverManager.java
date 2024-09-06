3fa510d3062c1bfd8db7328496443aa9c2f69ea8
karthik <karthik.arram@carbonplace.com>
2024-09-06 09:21:47 +0530

    Updated project wrt browserstack. Added respective dependency in pom file Added browstack yml file to the project Updated code in AndroidWebDriverManager wrt loading of properties

--- android/src/main/java/managers/AndroidWebDriverManager.java
+++ android/src/main/java/managers/AndroidWebDriverManager.java
@@ -10,6 +10,8 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.asserts.SoftAssert;
@@ -35,8 +37,43 @@ public class AndroidWebDriverManager {
 	private SoftAssert softAssert;
 	private Actions actions;
 	private Proxy proxy;
-	public  String downloadDirectoryPath;
-	AndroidDriver aDriver;
+	public String downloadDirectoryPath;
+	private AndroidDriver aDriver;
+
+
+	// Constructor to initialize the AndroidDriver instance
+	public AndroidWebDriverManager(AndroidDriver aDriver, ExtentTest test) {
+		this.aDriver = aDriver; // Assign the passed AndroidDriver to the class variable 'aDriver'
+		this.test = test; // Ensure test is passed and initialized
+
+		prop = new Properties(); // Initialize prop here
+		envprop = new Properties(); // Initialize envprop here
+		loadProperties();
+	}
+
+	// Method to get the AndroidDriver instance
+	public AndroidDriver getDriver() {
+		return aDriver; // Return the class variable 'aDriver'
+	}
+
+	private void loadProperties() {
+		try {
+			// Creating a File object for directory
+			File directoryPath = new File(System.getProperty("user.dir") + "/src/test/resources/androidproperties");
+			// List of all files and directories
+			File[] filesList = directoryPath.listFiles();
+			for (File file : filesList) {
+				FileInputStream propertyfiles = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/androidproperties/" + file.getName());
+				prop.load(propertyfiles);
+			}
+			String env = prop.getProperty("env") + ".properties";
+			FileInputStream envpropertyfile = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/androidproperties/" + env);
+			envprop.load(envpropertyfile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 
 	public AndroidWebDriverManager() {
 		// init the properties file
@@ -49,13 +86,11 @@ public class AndroidWebDriverManager {
 				// List of all files and directories
 				File filesList[] = directoryPath.listFiles();
 				for (File file : filesList) {
-					FileInputStream propertyfiles = new FileInputStream(
-							System.getProperty("user.dir") + "/src/test/resources/androidproperties/" + file.getName());
+					FileInputStream propertyfiles = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/androidproperties/" + file.getName());
 					prop.load(propertyfiles);
 				}
 				String env = prop.getProperty("env") + ".properties";
-				FileInputStream envpropertyfile = new FileInputStream(
-						System.getProperty("user.dir") + "/src/test/resources/androidproperties/" + env);
+				FileInputStream envpropertyfile = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/androidproperties/" + env);
 				envprop.load(envpropertyfile);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
@@ -65,87 +100,6 @@ public class AndroidWebDriverManager {
 		}
 	}
 
-
-
-	/**
-	 * This Method is called for opening the browser
-	 *
-     */
-	public void openApp(String deviceName, String automationType) throws MalformedURLException {
-		UiAutomator2Options options = new UiAutomator2Options();
-
-		switch (automationType) {
-			case "app":
-				setAppOptions(deviceName, options);
-				break;
-			case "browser":
-				setBrowserOptions(options);
-				break;
-			default:
-				throw new IllegalArgumentException("Invalid automation type: " + automationType);
-		}
-
-		wait(15000);
-
-		try {
-			String gridUrl = "http://localhost:4444/wd/hub";
-			aDriver = new AndroidDriver(new URL(gridUrl), options);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-			Assert.fail("Invalid URL for Selenium Grid Hub - " + e.getMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-			Assert.fail("Driver failed to start - " + e.getMessage());
-		}
-
-		if ("browser".equals(automationType)) {
-			aDriver.get(prop.getProperty("url"));
-		}
-
-		setDefaultImplicitlyWait();
-	}
-
-	private void setAppOptions(String deviceName, UiAutomator2Options options) {
-		File directoryPath = new File(System.getProperty("user.dir") + "/src/test/resources/MakeMyTrip.apk");
-
-		switch (deviceName) {
-			case "android11":
-				options.setDeviceName(prop.getProperty("android11DeviceName"));
-				options.setPlatformName(prop.getProperty("android11PlatformName"));
-				options.setPlatformVersion(prop.getProperty("android11PlatformVersion"));
-				options.setSystemPort(8205);
-				options.setApp(directoryPath.getAbsolutePath());
-				options.setCapability("uiautomator2ServerInstallTimeout", 600000);
-				options.setCapability("uiautomator2ServerLaunchTimeout", 90000);
-				options.setCapability("androidInstallTimeout", 600000);
-				options.setCapability("ignoreHiddenApiPolicyError", true);
-				options.setNoSign(true);
-				options.setFullReset(false);
-				break;
-			case "android09":
-				options.setDeviceName(prop.getProperty("android09DeviceName"));
-				options.setPlatformName(prop.getProperty("android09PlatformName"));
-				options.setPlatformVersion(prop.getProperty("android09PlatformVersion"));
-				options.setSystemPort(8207);
-				options.setApp(directoryPath.getAbsolutePath());
-				options.setCapability("uiautomator2ServerLaunchTimeout", 90000);
-				options.setCapability("uiautomator2ServerInstallTimeout", 600000);
-				options.setCapability("androidInstallTimeout", 600000);
-				options.setCapability("ignoreHiddenApiPolicyError", true);
-				options.setNoSign(true);
-				options.setFullReset(false);
-				break;
-			default:
-				throw new IllegalArgumentException("Invalid device name: " + deviceName);
-		}
-	}
-
-	private void setBrowserOptions(UiAutomator2Options options) {
-		options.setDeviceName(prop.getProperty("android09DeviceName"));
-		options.setPlatformName(prop.getProperty("android09PlatformName"));
-		options.setPlatformVersion(prop.getProperty("android09PlatformVersion"));
-		options.setCapability("browserName", "chrome");
-	}
 
 	/**
 	 * This Method is called for clicking on a webelement
@@ -163,12 +117,24 @@ public class AndroidWebDriverManager {
 		}
 	}
 
+	public void clickAndEnterText(String locatorKey, String text) {
+		try {
+			WebElement element = new WebDriverWait(aDriver, Duration.ofSeconds(30)).until(ExpectedConditions.elementToBeClickable(getElement(locatorKey, true)));
+			element.click();
+			element.sendKeys(text);
+		} catch (Exception ex) {
+			// Handle exception, maybe log it or report a failure
+			ex.printStackTrace();
+		}
+	}
+
+
 	/**
 	 * This Method is called to check whether a webelement is in enable mode to
 	 * click or type
 	 *
 	 * @param locatorkey Is the webelement(fetched from propertyfile)
-	 * @param assertype Is a boolean which tells what type of assertion should be
+	 * @param assertype  Is a boolean which tells what type of assertion should be
 	 *                   done in case of pass and failure whether hardassert or
 	 *                   sortassert should be done
 	 */
@@ -181,13 +147,13 @@ public class AndroidWebDriverManager {
 	 * click or type
 	 *
 	 * @param locatorkey Is the webelement(fetched from propertyfile)
-	 * @param assertype Is a boolean which tells what type of assertion should be
+	 * @param assertype  Is a boolean which tells what type of assertion should be
 	 *                   done in case of pass and failure whether hardassert or
 	 *                   sortassert should be done
 	 */
 	public void isEnabled(String locatorkey, boolean assertype, int timeout) {
 		//new WebDriverWait(aDriver, Duration.ofSeconds(timeout)).until(ExpectedConditions.visibilityOf(getElement(locatorkey, assertype)));
-				//visibilityOf(getElement(locatorkey, assertype)));
+		//visibilityOf(getElement(locatorkey, assertype)));
 		test.log(Status.INFO, "Verifying whether the mobileElement- " + locatorkey + " is enabled / clickable");
 		String actualValue = Boolean.toString(getElement(locatorkey, assertype).isEnabled());
 		String expectedValue = "true";
@@ -212,14 +178,12 @@ public class AndroidWebDriverManager {
 	public WebElement getElement(String locatorkey, boolean asserttype) {
 		WebElement e = null;
 		try {
-			if (locatorkey.endsWith("_xpath"))
-				e = aDriver.findElement(AppiumBy.xpath(prop.getProperty(locatorkey)));
-			else if (locatorkey.endsWith("_id"))
-				e = aDriver.findElement(AppiumBy.id(prop.getProperty(locatorkey)));
-			else if (locatorkey.endsWith("_name"))
-				e = aDriver.findElement(AppiumBy.name(prop.getProperty(locatorkey)));
-			else if (locatorkey.endsWith("_webtablexpath"))
-				e = aDriver.findElement(AppiumBy.xpath(locatorkey));
+			if (locatorkey.endsWith("_xpath")) e = aDriver.findElement(AppiumBy.xpath(prop.getProperty(locatorkey)));
+			else if (locatorkey.endsWith("_id")) e = aDriver.findElement(AppiumBy.id(prop.getProperty(locatorkey)));
+			else if (locatorkey.endsWith("_accessibilityid"))
+				e = aDriver.findElement(AppiumBy.accessibilityId(prop.getProperty(locatorkey)));
+			else if (locatorkey.endsWith("_name")) e = aDriver.findElement(AppiumBy.name(prop.getProperty(locatorkey)));
+			else if (locatorkey.endsWith("_webtablexpath")) e = aDriver.findElement(AppiumBy.xpath(locatorkey));
 
 			else {
 				System.out.println("Locatorykey is not correct" + " " + locatorkey);
@@ -247,11 +211,11 @@ public class AndroidWebDriverManager {
 		}
 	}
 
-		/**
-         * This Method is called for printing messages in the report
-         *
-         * @param msg Pass the message which needs to printed
-         */
+	/**
+	 * This Method is called for printing messages in the report
+	 *
+	 * @param msg Pass the message which needs to printed
+	 */
 	public void log(String msg) {
 		test.log(Status.INFO, msg);
 		System.out.println(msg);
@@ -270,11 +234,11 @@ public class AndroidWebDriverManager {
 		test.log(Status.FAIL, MarkupHelper.createLabel(failuremsg, ExtentColor.RED));// report failure in extent report
 		takeScreenShot();// Put screen-shot in report
 
-		softAssert.fail(failuremsg);// report failure in testng
-		if (stoponfailure == true) {
-			Reporter.getCurrentTestResult().getTestContext().setAttribute("CriticalFailure", "Y");
-			AssertAll();
-		}
+//		softAssert.fail(failuremsg);// report failure in testng
+//		if (stoponfailure == true) {
+//			Reporter.getCurrentTestResult().getTestContext().setAttribute("CriticalFailure", "Y");
+//			AssertAll();
+//		}
 	}
 
 	/**
@@ -307,8 +271,7 @@ public class AndroidWebDriverManager {
 			// get the dynamic folder name
 			FileUtils.copyFile(scrFile, new File(ExtentManager.screenshotFolderPath + "/" + screenshotFile));
 			// adding the screen-shot in extent reports
-			test.log(Status.INFO, "Screenshot-> "
-					+ test.addScreenCaptureFromPath("screenshots" + "/" + screenshotFile));
+			test.log(Status.INFO, "Screenshot-> " + test.addScreenCaptureFromPath("screenshots" + "/" + screenshotFile));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -317,7 +280,7 @@ public class AndroidWebDriverManager {
 
 	public void quit() {
 		wait(2000);
-		if(aDriver!=null) {
+		if (aDriver != null) {
 			aDriver.quit();
 		}
 	}
@@ -325,7 +288,7 @@ public class AndroidWebDriverManager {
 	public void setDefaultImplicitlyWait() {
 		setImplicitlyWait(40);
 	}
-	
+
 	public void setImplicitlyWait(int waitInSeconds) {
 		aDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(waitInSeconds));
 	}
